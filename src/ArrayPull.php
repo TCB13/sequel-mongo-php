7a2712e0dedd2bc2c9f7f50160e83f1f6800f064
TCB13 <tcb13@iklive.org>
2019-05-23 18:40:47 +0100

    Improved ArrayPull: pull strings from arrays or objects matching conditions from arrays of objects.

--- src/ArrayPull.php
+++ src/ArrayPull.php
@@ -15,6 +15,32 @@ class ArrayPull implements SpecialFunction
 
     public function asArray(): array
     {
+        // Remove individual value from array
+        // new ArrayPull("list", "user.123")
+        if (!is_array($this->value)) {
+            return [
+                "\$pull" => [
+                    $this->name => $this->value
+                ]
+            ];
+        }
+
+        // Remove array values from property
+        // new ArrayPull("list", ["user.123", "user.456"])
+        if (is_int(array_keys($this->value)[0])) {
+            return [
+                "\$pull" => [
+                    $this->name => [
+                        "\$in" => $this->value
+                    ]
+                ]
+            ];
+        }
+
+        // Remove objects from array with conditions
+        // new ArrayPull("list", ["id" => ["user.123", "user.456"]])
+        // new ArrayPull("list", ["id" => "user.123"])
+        // new ArrayPull("list", ["id" => "user.123", "name" => "joe"])
         foreach ($this->value as $key => &$value) {
             if (is_array($value)) {
                 $value = [
